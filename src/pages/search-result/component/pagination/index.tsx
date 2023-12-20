9e322972d4f0d754a9999cbe34a26c84596f41ff
KingOfPk <pawanlike12@gmail.com>
2023-12-20 11:51:48 +0530

    code change

--- src/pages/search-result/component/pagination/index.tsx
+++
@@ -1,9 +0,0 @@
-// components/MyPagination.tsx
-import React from 'react';
-import { Pagination } from 'antd';
-
-const MyPagination: React.FC = () => (
-  <Pagination defaultCurrent={6} total={500} />
-);
-
-export default MyPagination;
